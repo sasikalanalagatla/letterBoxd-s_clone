91cc571ebd46b14e0a5717b93b43769010c4c37e
Sasikala Nalagatla <sasikalanalagatla@gmail.com>
2026-02-26 14:56:42 +0530

    first commit

---
+++ src/main/java/com/clone/letterboxd/LetterboxdApplication.java
@@ -0,0 +1,13 @@
+package com.clone.letterboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterboxdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clone/letterboxd/LetterboxdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.letterboxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterboxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
