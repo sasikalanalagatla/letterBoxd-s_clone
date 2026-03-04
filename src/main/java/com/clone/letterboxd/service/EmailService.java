fc1bf0354d7d0b470c76350b797a67e8980e4daa
Sasikala Nalagatla <sasikalanalagatla@gmail.com>
2026-03-04 14:46:26 +0530

    modifying email to send forget password

--- src/main/java/com/clone/letterboxd/service/EmailService.java
+++ src/main/java/com/clone/letterboxd/service/EmailService.java
@@ -7,6 +7,7 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,7 +29,9 @@ public class EmailService {
             Email from = new Email("sasinalagatla30@gmail.com", "Letterboxd Clone");
             Email to = new Email(toEmail);
             String subject = "Letterboxd - Password Reset Request";
-            String body = "Hello,\n\n"
+
+            // Plain text version
+            String plainTextBody = "Hello,\n\n"
                     + "We received a request to reset your password. Click the link below to set a new password:\n\n"
                     + resetLink + "\n\n"
                     + "This link expires in 1 hour.\n\n"
@@ -36,8 +39,32 @@ public class EmailService {
                     + "Best regards,\n"
                     + "Letterboxd Team";
 
-            Content content = new Content("text/plain", body);
-            Mail mail = new Mail(from, subject, to, content);
+            // HTML version (Better deliverability)
+            String htmlBody = "<html><body>"
+                    + "<h3>Password Reset Request</h3>"
+                    + "<p>Hello,</p>"
+                    + "<p>We received a request to reset your password for your Letterboxd account. Click the button below to set a new password:</p>"
+                    + "<p><a href=\"" + resetLink + "\" style=\"background-color: #008100; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;\">Reset Password</a></p>"
+                    + "<p>If the button doesn't work, copy and paste this link into your browser:</p>"
+                    + "<p><a href=\"" + resetLink + "\">" + resetLink + "</a></p>"
+                    + "<p>This link will expire in 1 hour.</p>"
+                    + "<p>If you didn't request this, please ignore this email.</p>"
+                    + "<p>Best regards,<br>Letterboxd Team</p>"
+                    + "</body></html>";
+
+            Mail mail = new Mail();
+            mail.setFrom(from);
+            mail.setSubject(subject);
+            
+            // Add both plain text and HTML
+            Content textContent = new Content("text/plain", plainTextBody);
+            Content htmlContent = new Content("text/html", htmlBody);
+            mail.addContent(textContent);
+            mail.addContent(htmlContent);
+
+            Personalization personalization = new Personalization();
+            personalization.addTo(to);
+            mail.addPersonalization(personalization);
 
             SendGrid sg = new SendGrid(sendGridApiKey);
             Request request = new Request();
